27896b6f2c937a3fa41fc5fd531adbb722102933
juananturi <juan.anturi@misena.edu.co>
2023-08-08 16:44:55 -0500

    se modifica la ruta de las cajas nuevas para que direccione hacia la ruta indicada

--- js/main.js
+++ js/main.js
@@ -101,3 +101,28 @@
     
 })(jQuery);
 
+ // Obtener los elementos de radio buttons
+ const cartonDobleRadio = document.getElementById('#carton_doble');
+ const cartonSencilloRadio = document.getElementById('#carton_sencillo');
+
+ // Función para deseleccionar las opciones después de un minuto
+ function deseleccionarOpcion() {
+     cartonDobleRadio.checked = false;
+     cartonSencilloRadio.checked = false;
+ }
+
+ // Event listener para detectar cambios en las opciones
+ cartonDobleRadio.addEventListener('change', () => {
+     // Reiniciar el temporizador si se cambia la opción antes de que se cumpla el minuto
+     clearTimeout(window.temporizador);
+     // Establecer un nuevo temporizador para deseleccionar la opción después de un minuto
+     window.temporizador = setTimeout(deseleccionarOpcion, 60000);
+ });
+
+ cartonSencilloRadio.addEventListener('change', () => {
+     // Reiniciar el temporizador si se cambia la opción antes de que se cumpla el minuto
+     clearTimeout(window.temporizador);
+     // Establecer un nuevo temporizador para deseleccionar la opción después de un minuto
+     window.temporizador = setTimeout(deseleccionarOpcion, 60000);
+ });
+
